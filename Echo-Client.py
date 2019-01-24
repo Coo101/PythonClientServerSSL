7fcd634aca97a55baf417bc736c8e5c14cb2a371
Yonick Verlinden <yverlinden@example.com>
2019-01-24 12:20:29 +0100

    Base Echo Client-Server

--- Echo-Client.py
+++ Echo-Client.py
@@ -1,13 +1,32 @@
-#!/usr/bin/env python3
-
 import socket
+import sys
 
 HOST = '127.0.0.1'  # The server's hostname or IP address
 PORT = 65432        # The port used by the server
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(b'Hello, world')
-    data = s.recv(1024)
+# Create a TCP/IP socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-print('Received', repr(data))
+# Connect the socket to the port where the server is listening
+server_address = ('localhost', 10000)
+print >>sys.stderr, 'connecting to %s port %s' % server_address
+s.connect(server_address)
+try:
+
+    # Send data
+    message = 'This is the message.  It will be repeated.'
+    print >> sys.stderr, 'sending "%s"' % message
+    s.sendall(message)
+
+    # Look for the response
+    amount_received = 0
+    amount_expected = len(message)
+
+    while amount_received < amount_expected:
+        data = s.recv(16)
+        amount_received += len(data)
+        print >> sys.stderr, 'received "%s"' % data
+
+finally:
+    print >> sys.stderr, 'closing socket'
+    s.close()
